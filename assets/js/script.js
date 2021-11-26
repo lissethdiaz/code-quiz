c0084d6d58573e9d5cbc9a4437607f196fda43fe
Liss Diaz <lissdiaz15@gmail.com>
2021-11-25 23:38:49 -0700

    html and css set up

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var questions = [{
+    question: "The 'function' and 'var' are known as:",
+    correctAnswer: "Declaration Statements",
+    choiceA: "Keywords",
+    choiceB: "Declaration Statements",
+    choiceC: "Prototypes",
+    choiceD: "Data Types",
+},
+  {
+    question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    correctAnswer: "Scope",
+    choiceA: "Resstriction",
+    choiceB: "Output Level",
+    choiceC: "Scope",
+    choiceD: "Range",
+},
+  {
+    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+    correctAnswer: "Boolean",
+    choiceA: "Boolean",
+    choiceB: "RegExp",
+    choiceC: "Event",
+    choiceD: "Condition",
+},
+  {
+    question: "In JavaScript, what element is used to store multiple values in a single variable?",
+    correctAnswer: "Arrays",
+    choiceA: "Arrays",
+    choiceB: "Variables",
+    choiceC: "Functions",
+    choiceD: "Strings",
+},
+];
+
+var startBtn = document.querySelector(".start-btn");
+var quiz = document.querySelector(".quiz-card");
+var quizQuestion = document.querySelector("question");
+var choiceA = document.querySelector("#A");
+var choiceB = document.querySelector("#B");
+var choiceC = document.querySelector("#C");
+var choiceD = document.querySelector("#D");
+
+
+// var startQuiz = function() {
+//   console.log("start")
+//   for (var i = 0; i < questions.length; i++) {
+//   quizQuestion = questions[i];
+
+// };
+// }
+startBtn.addEventListener("click", ()=>{
+
+});
+
+
+
+
+
+// function startQuiz(questions) {
+//   var questions = document.querySelector(".questions");
+  
+//   for (var i = 0; i < questions.length; i++) {
+//     questions.InnerText = questions[i];
+//   };
+// }
+
