5c66396ebfe4945896091541d2aae577d6cdb3bf
Liss Diaz <lissdiaz15@gmail.com>
2021-12-06 13:59:20 -0700

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var questions = [{
     question: "The 'function' and 'var' are known as:",
-    correctAnswer: "Declaration Statements",
+    correctAnswer: "chB",
     choiceA: "Keywords",
     choiceB: "Declaration Statements",
     choiceC: "Prototypes",
@@ -8,7 +8,7 @@ var questions = [{
 },
   {
     question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
-    correctAnswer: "Scope",
+    correctAnswer: "chC",
     choiceA: "Resstriction",
     choiceB: "Output Level",
     choiceC: "Scope",
@@ -16,7 +16,7 @@ var questions = [{
 },
   {
     question: "What is a JavaScript element that represents either TRUE or FALSE values?",
-    correctAnswer: "Boolean",
+    correctAnswer: "chA",
     choiceA: "Boolean",
     choiceB: "RegExp",
     choiceC: "Event",
@@ -24,43 +24,95 @@ var questions = [{
 },
   {
     question: "In JavaScript, what element is used to store multiple values in a single variable?",
-    correctAnswer: "Arrays",
+    correctAnswer: "chA",
     choiceA: "Arrays",
     choiceB: "Variables",
     choiceC: "Functions",
     choiceD: "Strings",
-},
-];
+}];
 
-var startBtn = document.querySelector(".start-btn");
-var quiz = document.querySelector(".quiz-card");
-var quizQuestion = document.querySelector("question");
-var choiceA = document.querySelector("#A");
-var choiceB = document.querySelector("#B");
-var choiceC = document.querySelector("#C");
-var choiceD = document.querySelector("#D");
+var quizInfo = document.querySelector("#quiz-info");
+var startBtn = document.querySelector("#start-btn");
 
+var timeLeft = document.querySelector("#time-left")
 
-// var startQuiz = function() {
-//   console.log("start")
-//   for (var i = 0; i < questions.length; i++) {
-//   quizQuestion = questions[i];
+var quiz = document.querySelector("#quiz");
 
-// };
-// }
-startBtn.addEventListener("click", ()=>{
+var question = document.querySelector("#question");
 
+var chA = document.querySelector("#chA");
+var chB = document.querySelector("#chB");
+var chC = document.querySelector("#chC");
+var chD = document.querySelector("#chD");
+var choice = document.querySelector("#choice");
+var choices = document.querySelector("#choices");
+
+var highScore = document.querySelector("#highscore");
+var yourScore = document.querySelector("#score");
+var submitScore = document.querySelector("#submit-btn");
+var initials = document.querySelector("#initials")
+
+var i = 0
+var countDown = 60
+var time;
+
+startBtn.addEventListener("click", () => {
+  quizInfo.style.display = "none";
+  quiz.style.display = "block";
+  time = setInterval(timer, 1000)
 });
 
+var timer = function() {
+  console.log(`Timer called ${--countDown}`);
+  if(countDown === 0) {
+    clearInterval(time)
+    endQuiz()
+  }
 
 
+  timeLeft.innerText = countDown
+}
 
+var quizQuestion = function() {
+    question.textContent = questions[i].question;
+    chA.textContent = questions[i].choiceA;
+    chB.textContent = questions[i].choiceB;
+    chC.textContent = questions[i].choiceC;
+    chD.textContent = questions[i].choiceD;
 
-// function startQuiz(questions) {
-//   var questions = document.querySelector(".questions");
+  };
+
+quizQuestion();
+
+  choices.addEventListener("click", (event) => {
+    i++;
+    if(i < questions.length) {
+      var correctChoice = questions[i-1].correctAnswer
+      var userChoice = event.id
+      if(userChoice != correctChoice) {
+        countDown = countDown -5
+      }
+      
+      quizQuestion()
+    } else {
+      highScore.style.display = "block"
+      quiz.style.display = "none"
+      clearInterval(time)
+      endQuiz()
+    }
+  })
   
-//   for (var i = 0; i < questions.length; i++) {
-//     questions.InnerText = questions[i];
-//   };
-// }
+  var endQuiz = function() {
+    yourScore.textContent = countDown
+    countDown = 60
+    i = 0
+  }
+
+  submitScore.addEventListener("click", () => {
+    localStorage.setItem(initials.value, yourScore.textContent)
+
+  })
+
+
+
 
